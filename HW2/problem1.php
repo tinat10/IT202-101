4761477b46cd969d27e031b526aeffe4d9eefc6b
Tina Thai <tinathai05@gmail.com>
2023-09-13 20:16:22 -0400

    final commit today 09/13

--- HW2/problem1.php
+++ HW2/problem1.php
@@ -4,10 +4,22 @@ $a2 = [0, 1, 3, 5, 7, 9, 2, 4, 6, 8, 10];
 $a3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 $a4 = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
 function processArray($arr) {
-    echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
-    echo "<br>Odds output:<br>";
+    // echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
+    // echo "<br>Odds output:<br>";
+
+    $final = [];
+
+# Problem 1: Echo odd numbers
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
     //TODO add logic here to echo out only odd values
+
+    foreach($arr as $item) {
+        if ($item % 2 == 1) {
+            echo $item;
+            array_push($final, $item);
+        }
+
+    }
 
 }
 echo "Problem 1: Odd Output<br>";
