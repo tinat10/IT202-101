7c4118d3fdd47878e10ed26bda041df15d708ff0
Tina Thai <tinathai05@gmail.com>
2023-11-26 23:51:45 -0500

    flash message

--- M1/create_role.php
+++ M1/create_role.php
@@ -4,7 +4,7 @@ require(__DIR__ . "/partials/nav.php");
 //require_once(__DIR__ . "/partials/flash.php");
 
 if (!has_role("Admin")) {
-    ///flash("You don't have permission to view this page", "warning");
+    flash("You don't have permission to view this page", "warning");
     //die(header("Location: " . get_url("home.php")));
 }
 
