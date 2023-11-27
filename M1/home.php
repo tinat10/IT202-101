440ecfdc969c3d3786525ae1bfa54c78c293add5
Tina Thai <tinathai05@gmail.com>
2023-11-27 13:07:36 -0500

    home edits

--- M1/home.php
+++ M1/home.php
@@ -5,12 +5,13 @@ require(__DIR__ . "/partials/nav.php");
 <?php
 if (is_logged_in()) {
     echo "Welcome, " . get_username();
+    echo "<pre>" . var_export($_SESSION, true) . "</pre>";
+    echo has_role('Admin');
 } else {
     echo "You're not logged in";
 }
 //shows session info
-echo "<pre>" . var_export($_SESSION, true) . "</pre>";
-echo has_role('Admin');
+
 ?>
 
 <style>
